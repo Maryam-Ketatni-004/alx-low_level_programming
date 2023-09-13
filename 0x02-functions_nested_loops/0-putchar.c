be01198f3c7685d407049bea30a4011f58984294
Maryam-Ketatni-004 <maryamketatni00.com>
2023-09-13 16:38:30 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+/**
+*main - putchar
+*
+*Return:0(success)
+*/
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
+
+
+
