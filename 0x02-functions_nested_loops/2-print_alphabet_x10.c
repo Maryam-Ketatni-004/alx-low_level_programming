d3bb649f1ec7cb751d76d1c5930118cf0c447592
Maryam-Ketatni-004 <maryamketatni00.com>
2023-09-13 18:49:49 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-* print_alphabet_x1 - prints teen times the alphabet
+* print_alphabet_x10 - print alphabet teen times
 *
 * Return: alwyas void
 */
 void print_alphabet_x10(void)
 {
 	char ch;
+	int a;
 
-	for (ch = 48; ch <= 57; ch++)
+	for (a = 0; a < 10; a++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
