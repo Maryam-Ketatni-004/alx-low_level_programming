61497b0288c3734f5eacde6d6010fba1c800f414
Maryam-Ketatni-004 <maryamketatni00.com>
2023-10-07 18:59:37 -0700

    libr

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,15 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character.
- * @ch: the character checked
- * Return:1 if lowercase, otherwise 0.
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
-int _islower(int ch)
+int _islower(int c)
 {
-	if (ch >= 'a' && ch <= 'z')
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
