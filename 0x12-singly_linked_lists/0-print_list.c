36ae07fb997a6d4635c2a3b0477d33ba77626e0e
Maryam-Ketatni-004 <maryamketatni00.com>
2023-10-20 13:56:59 -0700

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,28 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list
- * @h: pointer to the list
- * Return: the number of nodes
+ * print_list - print elements of the list.
+ *
+ * @h: a list_t variable.
+ * Return: the numbre of nodes.
  */
 
 size_t print_list(const list_t *h)
 {
-	const list_t *cursor = h;
-	size_t count = 0;
+	size_t j = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		if (cursor->str != NULL)
-			printf("[%d] %s\n", cursor->len, cursor->str);
-		else
+		if ((h->str) == NULL)
+		{
 			printf("[0] (nil)\n");
-		count += 1;
-		cursor = cursor->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		j++;
 	}
-	return (count);
+	return (j);
 }
